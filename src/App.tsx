4bd03a8ba91eb1e15bde4eb3be70886b6bf43ebc
João Mangueira <jamjs7@gmail.com>
2021-10-21 10:50:15 -0300

    enviando fotos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, FormEvent } from 'react';
 import * as Components from './App.styles';
 import * as Photos from './service/photos';
 import { Photo } from './types/Photo';
 import { PhotoItem } from './components/PhotoItem';
 
 const App = () => {
+    const [uploading, setUploading] = useState(false);
     const [loading, setLoading] = useState(false);
     const [photos, setPhotos] = useState<Photo[]>([]);
 
@@ -18,8 +19,24 @@ const App = () => {
         getPhotos();
     }, []);
 
-    const handleFormSubmit = () => {
+    const handleFormSubmit = async (e: FormEvent<HTMLFormElement> ) => {
+        e.preventDefault();
 
+        const formData = new FormData(e.currentTarget);
+        const file = formData.get('image') as File;
+        if(file && file.size > 0) {
+            setUploading(true);
+            let result = await Photos.insert(file);
+            setUploading(false);
+
+            if(result instanceof Error) {
+                alert(`${result.name} - ${result.message}`)
+            } else {
+                let newPhotoList = [...photos];
+                newPhotoList.push(result);
+                setPhotos(newPhotoList);
+            }
+        }
     }
 
     return (
@@ -30,6 +47,7 @@ const App = () => {
               <Components.UploadForm method="POST" onSubmit={handleFormSubmit}>
                   <input type="file" name="image"/>
                   <input type="submit" value="Enviar"/>
+                  {uploading && "Enviando..."}
               </Components.UploadForm>
 
               {loading &&
