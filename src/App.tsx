edd5b27e551195943422a31e126f5ecde66fe708
João Mangueira <jamjs7@gmail.com>
2021-10-21 10:35:39 -0300

    visual do upload ponto

--- src/App.tsx
+++ src/App.tsx
@@ -17,32 +17,44 @@ const App = () => {
 
         getPhotos();
     }, []);
-  return (
-    <Components.Container>
-      <Components.Area>
-        <Components.Header>Galeria de Fotos</Components.Header>
-          {loading &&
-            <Components.ScreenWarning>
-                <div className="emoji">🤚</div>
-                <div>Carregando...</div>
-            </Components.ScreenWarning>
-          }
-          {!loading && photos.length > 0 &&
-              <Components.PhotoList>
-                  {photos.map((item, index) => (
-                    <PhotoItem key={index} url={item.url} name={item.name}/>
-                  ))}
-              </Components.PhotoList>
-          }
 
-          {!loading && photos.length === 0 &&
-              <Components.ScreenWarning>
-                  <div className="emoji">😞</div>
-                  <div>Não há foto cadastradas</div>
-              </Components.ScreenWarning>
-          }
-      </Components.Area>
-    </Components.Container>
+    const handleFormSubmit = () => {
+
+    }
+
+    return (
+        <Components.Container>
+            <Components.Area>
+            <Components.Header>Galeria de Fotos</Components.Header>
+
+              <Components.UploadForm method="POST" onSubmit={handleFormSubmit}>
+                  <input type="file" name="image"/>
+                  <input type="submit" value="Enviar"/>
+              </Components.UploadForm>
+
+              {loading &&
+                <Components.ScreenWarning>
+                    <div className="emoji">🤚</div>
+                    <div>Carregando...</div>
+                </Components.ScreenWarning>
+              }
+              {!loading && photos.length > 0 &&
+                  <Components.PhotoList>
+                      {photos.map((item, index) => (
+                        <PhotoItem key={index} url={item.url} name={item.name}/>
+                      ))}
+                  </Components.PhotoList>
+              }
+
+              {!loading && photos.length === 0 &&
+                  <Components.ScreenWarning>
+                      <div className="emoji">😞</div>
+                      <div>Não há foto cadastradas</div>
+                  </Components.ScreenWarning>
+              }
+
+            </Components.Area>
+        </Components.Container>
   );
 }
 
